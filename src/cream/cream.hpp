f99d0f68ddfa53f069554ca793a1edfe39e5abbb
kz04px <6536046+kz04px@users.noreply.github.com>
2019-07-17 23:03:08 +0100

    Add a single header to include

---
+++ src/cream/cream.hpp
@@ -0,0 +1,15 @@
+#ifndef CREAM_HPP
+#define CREAM_HPP
+
+#include "engine.hpp"
+#include "events/keyboard-event.hpp"
+#include "events/mouse-event.hpp"
+#include "events/window-event.hpp"
+#include "input.hpp"
+#include "keys.hpp"
+#include "layers/layer.hpp"
+#include "models/mesh.hpp"
+#include "rendering/camera.hpp"
+#include "rendering/instance.hpp"
+
+#endif
